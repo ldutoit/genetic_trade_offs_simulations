09945076627c3c3b71f86e234e6c1a3023302dec
Ludovic Dutoit <dutoit.ludovic@gmail.com>
2025-03-12 19:40:47 +1300

    Create supp_run_simulations.R

---
+++ SuppFigure5/supp_run_simulations.R
@@ -0,0 +1,34 @@
+### This file runs the simulations analyses for 
+### supplementary figure 5, including the assymetric case.
+
+################################################
+## Wright - Fisher simulations
+################################################
+
+source('./suppFig-functions.R')
+
+################################################
+## Run simulations
+################################################
+
+dir<-"results/" # output directory , created if it does not exist. Finish by /
+
+
+#create output directory if needed
+if (!dir.exists(dir)) {
+  dir.create(dir)
+  message(paste(dir,"Directory created."))
+} else {
+  warning("Directory already exists.")
+}
+
+
+# Run the simulations for z.1 = z.2
+
+ fixSA.netbenefit.sym(n=50, n.ben.tofix = 10^4, max.mutations = 10^7, z=1, m=0.05, R = 1,    output.dir=dir, file.name="all")
+ fixSA.netbenefit.sym(n=50, n.ben.tofix = 10^4, max.mutations = 10^7, z=1, m=0.05, R = 1.02, output.dir=dir, file.name="all")
+ fixSA.netbenefit.sym(n=50, n.ben.tofix = 10^4, max.mutations = 10^7, z=1, m=0.05, R = 0.98, output.dir=dir, file.name="all")
+ #Run the simulations for z.1 =/= z.2
+ fixSA.netbenefit.asym(n=50, n.ben.tofix = 10^4, max.mutations = 10^7,  m=0.05, R = 1,    output.dir=dir, file.name="all")
+ fixSA.netbenefit.asym(n=50, n.ben.tofix = 10^4, max.mutations = 10^7,  m=0.05, R = 1.02, output.dir=dir, file.name="all")
+ fixSA.netbenefit.asym(n=50, n.ben.tofix = 10^4, max.mutations = 10^7,  m=0.05, R = 0.98, output.dir=dir, file.name="all")
